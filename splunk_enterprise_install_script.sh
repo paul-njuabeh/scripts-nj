889a7bf5bb59591ead2437dc3ddb90f08100e072
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-10-17 19:27:08 -0700

    Update splunk_enterprise_install_script.sh

--- splunk_enterprise_install_script.sh
+++ splunk_enterprise_install_script.sh
@@ -23,7 +23,7 @@ echo "Adding default Splunk Admin user and password"
 cat > /opt/splunk/etc/system/local/user-seed.conf << 'EOF'
 [user_info]
 USERNAME = admin
-PASSWORD = P@ssw0RD123
+PASSWORD = 12345
 EOF
 
 cat > /opt/splunk/etc/system/local/server.conf << 'EOF'
