fb4b6287432be9f88a3257e82f169543f5db4300
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-11-27 18:05:56 -0800

    Update and rename Splunk_Installer_script_112723 to Splunk_Installer_script_112723.sh

--- Splunk_Installer_script_112723.sh
+++ Splunk_Installer_script_112723.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Ensure the script is run with root privileges
+## Ensure the script is run with root privileges
 if [[ $EUID -ne 0 ]]; then
     echo "###### This script must be run as root #######"
     exit 1
